7cd9a3aaba40db72772d601b26399b3dac299932
emilykrohn <emilykrohn2@gmail.com>
2026-03-14 14:37:25 -0700

    Add comments to calendar.js

--- calendar.js
+++ calendar.js
@@ -1,23 +1,30 @@
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const today = new Date();
 
-current_month = today.getMonth();
-current_year = today.getFullYear()
-current_day = today.getDay();
-updateCalendar();
+// Set date to the present day the user is accessing the application on
+var current_month = today.getMonth();
+var current_year = today.getFullYear()
+var current_day = today.getDay();
 
+// Keeps track of the day that has been just edited, used in saveForm()
 var current_day_edited = 0;
+// Keeps track of day already selected so it can be unselected when another day is selected
 var previous_selected = null;
 
+// Create initial calendar
+updateCalendar();
+
+
+// Update the calendar to the current date the user has moved to
 function updateCalendar() {
+    // Update text at top of calendar of homepage to the current month and year
     document.getElementById("monthAndYear").innerHTML = String(months[current_month]) + ' ' + String(current_year);
     
-    function daysInMonth(month, year) { // 1 - January
-        return new Date(year, month , 0).getDate();
-    }
     daysInCurrentMonth = daysInMonth(current_month + 1, current_year, 0);
     firstWeekDay = new Date(current_year, current_month, 1).getDay() + 1
+    // Counts the current day that will be written to the calendar
     day_counter = 0
+    // Writes blank spaces for days that do not belong to the current month the user is looking at
     for (var i = 1; i <= 37; i++) {
         if (i >= firstWeekDay && i < daysInCurrentMonth + firstWeekDay) {
             day_counter += 1;
@@ -26,8 +33,10 @@ function updateCalendar() {
             document.getElementById(String(i)).innerHTML = '&nbsp;';
         }
     }
+
     const nextButton = document.getElementsByClassName("next");
     const previousButton = document.getElementsByClassName("previous");
+    // Remove the change in color from the currently pressed date on the calendar when the next or previous button is pressed
     nextButton[0].addEventListener("click", () => {
         if (previous_selected !== null) {
             document.getElementById(String(previous_selected)).classList.remove("currentSquare");
@@ -42,39 +51,53 @@ function updateCalendar() {
         previous_selected = null;
     });
 
+    // Store all dates of the current month in dates
     const dates = document.getElementsByClassName("squares");
-    var current_day = 0;
+    // Display form when day is pressed on calendar
     for (var i = 0; i < dates.length; i++) {
+        // Add event listeners to all dates on calendar
         dates[i].addEventListener("click", (date) => {
             current_day = date.target.id - firstWeekDay + 1;
             current_day_edited = current_day;
-                if (current_day > 0 && current_day <= daysInCurrentMonth) {
-                    document.getElementById("form").style.display = "block";
-                    document.getElementById("formDate").innerHTML = "Date: " + String(current_month + 1) + "-" + String(current_day) + "-" + String(current_year);
+            // Only shows form if day clicked is in the current month being viewed
+            if (current_day > 0 && current_day <= daysInCurrentMonth) {
+                // Make form visible
+                document.getElementById("form").style.display = "block";
+                // Change the formDate Label to display the current date of the day selected
+                document.getElementById("formDate").innerHTML = "Date: " + String(current_month + 1) + "-" + String(current_day) + "-" + String(current_year);
 
-                    if (previous_selected !== null) {
-                        document.getElementById(String(previous_selected)).classList.remove("currentSquare");
-                    }
-                    document.getElementById(String(date.target.id)).classList.add("currentSquare");
-                    previous_selected = date.target.id;
+                // If a previous day has been selected, remove the currentSquare class that makes that square a different color
+                if (previous_selected !== null) {
+                    document.getElementById(String(previous_selected)).classList.remove("currentSquare");
+                }
 
-                    if (localStorage.getItem(String(current_year) + "-" + String(current_month + 1) + "-" + String(current_day)) !== null) {
-                        var dateHours = JSON.parse(localStorage.getItem(current_year + "-" + (current_month + 1) + "-" + current_day));
-                        document.getElementById("listening").innerHTML = '<label for="Listening" id="listening">Listening: ' + dateHours["listeningHours"] + " hour(s) and " + dateHours["listeningMinutes"] + " minute(s)</label><br>";
-                        document.getElementById("reading").innerHTML = '<label for="Reading" id="reading">Reading: ' + dateHours["readingHours"] + " hour(s) and " + dateHours["readingMinutes"] + " minute(s)</label><br>";
-                        document.getElementById("writing").innerHTML = '<label for="Writing" id="writing">Writing: ' + dateHours["writingHours"] + " hour(s) and " + dateHours["writingMinutes"] + " minute(s)</label><br>";
-                        document.getElementById("speaking").innerHTML = '<label for="Speaking" id="speaking">Speaking: ' + dateHours["speakingHours"] + " hour(s) and " + dateHours["speakingMinutes"] + " minute(s)</label><br>";
-                        document.getElementById("totalTimeRecorded").innerHTML = "<p>Total: " + dateHours["totalHours"] + " Hour(s) and " + dateHours["totalMinutes"] + " Minute(s)<\p>";
-                    } else {
-                        document.getElementById("listening").innerHTML = "<p>Listening: 0 hour(s) and 0 minute(s)</p>";
-                        document.getElementById("reading").innerHTML = "<p>Reading: 0 hour(s) and 0 minute(s)</p>";
-                        document.getElementById("writing").innerHTML = "<p>Writing: 0 hour(s) and 0 minute(s)</p>";
-                        document.getElementById("speaking").innerHTML = "<p>Speaking: 0 hour(s) and 0 minute(s)</p>";
-                        document.getElementById("totalTimeRecorded").innerHTML = "<p>Total: 0 Hour(s) and 0 Minutes</p>";
-                    }
+                // Add the currentSquare class to the current day
+                document.getElementById(String(date.target.id)).classList.add("currentSquare");
+                // Update the prevously selected day to the current
+                previous_selected = date.target.id;
+
+                // If current day selected has data already stored in local storage, display that information
+                if (localStorage.getItem(String(current_year) + "-" + String(current_month + 1) + "-" + String(current_day)) !== null) {
+                    var dateHours = JSON.parse(localStorage.getItem(current_year + "-" + (current_month + 1) + "-" + current_day));
+                    document.getElementById("listening").innerHTML = '<label for="Listening" id="listening">Listening: ' + dateHours["listeningHours"] + " hour(s) and " + dateHours["listeningMinutes"] + " minute(s)</label><br>";
+                    document.getElementById("reading").innerHTML = '<label for="Reading" id="reading">Reading: ' + dateHours["readingHours"] + " hour(s) and " + dateHours["readingMinutes"] + " minute(s)</label><br>";
+                    document.getElementById("writing").innerHTML = '<label for="Writing" id="writing">Writing: ' + dateHours["writingHours"] + " hour(s) and " + dateHours["writingMinutes"] + " minute(s)</label><br>";
+                    document.getElementById("speaking").innerHTML = '<label for="Speaking" id="speaking">Speaking: ' + dateHours["speakingHours"] + " hour(s) and " + dateHours["speakingMinutes"] + " minute(s)</label><br>";
+                    document.getElementById("totalTimeRecorded").innerHTML = "<p>Total: " + dateHours["totalHours"] + " Hour(s) and " + dateHours["totalMinutes"] + " Minute(s)<\p>";
+                } else {
+                    document.getElementById("listening").innerHTML = "<p>Listening: 0 hour(s) and 0 minute(s)</p>";
+                    document.getElementById("reading").innerHTML = "<p>Reading: 0 hour(s) and 0 minute(s)</p>";
+                    document.getElementById("writing").innerHTML = "<p>Writing: 0 hour(s) and 0 minute(s)</p>";
+                    document.getElementById("speaking").innerHTML = "<p>Speaking: 0 hour(s) and 0 minute(s)</p>";
+                    document.getElementById("totalTimeRecorded").innerHTML = "<p>Total: 0 Hour(s) and 0 Minutes</p>";
+                }
             }
         });
     }
+}
+
+function daysInMonth(month, year) { // 1 - January
+    return new Date(year, month , 0).getDate();
 }
 
 function previousMonth() {
